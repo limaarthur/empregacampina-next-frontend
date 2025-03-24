6d189fe1a990d242ddd637acfd49cf6dd029c4db
limaarthur <artglima.dev@gmail.com>
2025-03-24 15:58:33 -0300

    feat: creation application pages and routes

---
+++ app/vagas/cadastro/page.tsx
@@ -0,0 +1,5 @@
+export default function Cadastro() {
+  return (
+    <h1>Cadastro de vaga</h1>
+  );
+}
---
+++ app/vagas/page.tsx
@@ -0,0 +1,5 @@
+export default function Vagdas() {
+  return (
+    <h1>Listagem de vagas</h1>
+  );
+}
